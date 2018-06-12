1e70dc90972dd08e270edf53a440172b35d08297
Joe Andaverde <joe.andaverde@smrxt.com>
2018-06-11 22:15:55 -0500

    Initial go at a lexer

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, from tinydb!")
+	scanner := bufio.NewScanner(bufio.NewReader(os.Stdin))
+
+	onSemicolon := func(data []byte, atEOF bool) (advance int, token []byte, err error) {
+		for i := 0; i < len(data); i++ {
+			if data[i] == ';' {
+				return i + 1, data[:i], nil
+			}
+		}
+
+		return 0, data, bufio.ErrFinalToken
+	}
+
+	scanner.Split(onSemicolon)
+
+	for scanner.Scan() {
+		runLexer(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading input:", err)
+	}
+}
+
+func runLexer(text string) {
+	l := lex("TinySql", text)
+
+	for {
+		i := l.nextItem()
+
+		fmt.Println(i)
+
+		if i.token == tsqlEOF || i.token == tsqlError {
+			break
+		}
+	}
+
+	fmt.Println("-----")
 }
