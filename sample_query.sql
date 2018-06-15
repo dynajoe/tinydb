9cfa8b70a8c6f3bd89dc53d1ddb29a7ccfd5c5fd
Joe Andaverde <joe.andaverde@gmail.com>
2018-06-14 21:25:47 -0500

    Lex strings and operators.

--- sample_query.sql
+++ sample_query.sql
@@ -7,4 +7,4 @@ INSERT INTO person ( name, favorite_number ) VALUES ( joe, 1337 );
 INSERT INTO person ( name, favorite_number ) VALUES ( justin, 31337 );
 INSERT INTO person ( name, favorite_number ) VALUES ( stephen, 42 );
 
-SELECT * FROM person;
+SELECT 'joe' FROM person;
