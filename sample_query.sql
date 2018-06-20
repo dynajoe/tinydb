47898b4254ff168129cab81dfc90473c470efd21
Joe Andaverde <joe.andaverde@smrxt.com>
2018-06-20 17:22:17 -0500

    Cross joins with filters

--- sample_query.sql
+++ sample_query.sql
@@ -1,3 +1,3 @@
-SELECT *
-FROM person, person p
-WHERE name = 'joe' OR true
+SELECT x.name, *
+FROM person, person p, person x
+WHERE person.name = 'joe' AND p.name = 'stephen' AND x.name = 'justin' OR x.name = 'justin'
