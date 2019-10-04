8ca4c74054510e8ac08d7b4f13884fc4a0ee5366
Joe Andaverde <joe.andaverde@gmail.com>
2019-10-04 18:25:14 -0500

    add more tests

--- main_test.go
+++ main_test.go
@@ -2,8 +2,32 @@ package main
 
 import "testing"
 
-func TestParse(t *testing.T) {
+func TestSelectStar(t *testing.T) {
 	selectStatement := Parse("SELECT * FROM foo")
+
+	if selectStatement == nil {
+		t.Errorf("parsing select statement failed")
+	}
+}
+
+func TestSelectColumns(t *testing.T) {
+	selectStatement := Parse("SELECT a, b FROM foo")
+
+	if selectStatement == nil {
+		t.Errorf("parsing select statement failed")
+	}
+}
+
+func TestSelectFromMultipleTables(t *testing.T) {
+	selectStatement := Parse("SELECT a, b FROM foo, bar")
+
+	if selectStatement == nil {
+		t.Errorf("parsing select statement failed")
+	}
+}
+
+func TestSelectWhereClause(t *testing.T) {
+	selectStatement := Parse("SELECT a, b FROM foo, bar WHERE a = 1")
 
 	if selectStatement == nil {
 		t.Errorf("parsing select statement failed")
