a598b761dfbea4e476815c9a8fd5ba3943a12fa7
Joe Andaverde <joe.andaverde@hashicorp.com>
2020-04-17 11:23:53 -0500

    Parsing record from file and extracting table definitions for insert

--- engine/admin.go
+++ engine/admin.go
@@ -1,21 +1,41 @@
 package engine
 
 import (
+	"errors"
+
+	"github.com/joeandaverde/tinydb/ast"
+	"github.com/joeandaverde/tinydb/internal/btree"
 	"github.com/joeandaverde/tinydb/internal/storage"
 )
 
-func (e *Engine) GetTableDefinition(name string) (TableDefinition, error) {
+func (e *Engine) GetTableDefinition(name string) (*TableDefinition, error) {
 	pageOne, err := e.Pager.Read(1)
 	if err != nil {
-		return TableDefinition{}, err
+		return nil, err
 	}
 
-	_ = storage.BTreeFromPage(pageOne)
-
-	return TableDefinition{
-		Name: name,
-		Columns: []ColumnDefinition{
-			{Name: "", Type: "", PrimaryKey: false},
-		},
+	bt := storage.BTreeFromPage(pageOne)
+	tableDefinitionItem := bt.Find(&btree.StringItem{Key: name})
+	if tableDefinitionItem == nil {
+		return nil, errors.New("table not found")
+	}
+	record := tableDefinitionItem.(*btree.StringItem).Data.(*storage.Record)
+	createSQL := record.Fields[4].Data.(string)
+	stmt, err := ast.Parse(createSQL)
+	if err != nil {
+		return nil, err
+	}
+	var cols []ColumnDefinition
+	for _, c := range stmt.(*ast.CreateTableStatement).Columns {
+		cols = append(cols, ColumnDefinition{
+			Name:       c.Name,
+			Type:       c.Type,
+			Offset:     0,
+			PrimaryKey: false,
+		})
+	}
+	return &TableDefinition{
+		Name:    name,
+		Columns: cols,
 	}, nil
 }
